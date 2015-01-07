50f96abba03fd956ef9e30d1890b2492408992c4
John Lapeyre <john.lapeyre@gmail.com>
2015-01-07 20:24:00 +0100

    rearrange tests

--- test/lambertw_test.jl
+++ test/lambertw_test.jl
@@ -1,5 +1,10 @@
-#@test lambertw(NaN) === NaN
+@test_throws DomainError lambertw(-2.0,0)
+@test_throws DomainError lambertw(-2.0,-1)
+@test_throws ErrorException lambertw(-2.0,1)
+@test lambertw(0,-1) == lambertw(0.0,-1) == -Inf
+@test lambertw(-1/e,0) == lambertw(-1/e,-1) == -1
 @test_throws DomainError lambertw(NaN) 
+
 @test lambertw(complex(Inf,1),0) == complex(Inf,1)
 @test lambertw(complex(Inf,0),1) == complex(Inf,2pi)
 @test lambertw(complex(-Inf,0),1) == complex(Inf,3pi)
@@ -24,12 +29,6 @@ end
 
 # test the expansion about pole for k=-1
 @test lambertwbp(1e-20,-1) - 1 - lambertw(-BigFloat(1)/big(e)+ BigFloat(1)/BigFloat(10)^BigFloat(20),-1) < 1e-16
-
-@test_throws DomainError lambertw(-2.0,0)
-@test_throws DomainError lambertw(-2.0,-1)
-@test_throws ErrorException lambertw(-2.0,1)
-@test lambertw(0,-1) == lambertw(0.0,-1) == -Inf
-@test lambertw(-1/e,0) == lambertw(-1/e,-1) == -1
 
 # Fails unless we offset the starting point slightly before root finding.
 @test abs(lambertw(-1.0/e  + 0im,-1)) == 1
