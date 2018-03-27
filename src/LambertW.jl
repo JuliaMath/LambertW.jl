9b96e7a637ee0cf0d0316f3f4287b3f8c9f4261b
John Lapeyre <lapeyre.math122a@gmail.com>
2018-03-28 00:11:43 +0200

    reuse doc string for omega

--- src/LambertW.jl
+++ src/LambertW.jl
@@ -215,29 +215,7 @@ julia> big(omega)
 ```
 """
 const ω = Irrational{:ω}()
-
-# Repeat the documentation because v0.4 apparently does not allow ω, omega.
-doc"""
-    omega
-    ω
-
-A constant defined by `ω exp(ω) = 1`.
-
-```jldoctest
-julia> ω
-ω = 0.5671432904097...
-
-julia> omega
-ω = 0.5671432904097...
-
-julia> ω * exp(ω)
-1.0
-
-julia> big(omega)
-5.67143290409783872999968662210355549753815787186512508135131079223045793086683e-01
-```
-"""
-const omega = ω
+@doc (@doc ω) omega = ω
 
 convert(::Type{BigFloat}, ::Irrational{:ω}) = omega_const(BigFloat)
 convert(::Type{Float64}, ::Irrational{:ω}) = omega_const_
