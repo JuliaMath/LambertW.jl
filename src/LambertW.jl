91fc26ceac9ff4f402b13312cc5bec94cc8493d9
John Lapeyre <lapeyre.math122a@gmail.com>
2018-04-18 21:51:47 +0200

    comment on BigFloat allocation

--- src/LambertW.jl
+++ src/LambertW.jl
@@ -12,6 +12,7 @@ import Compat.MathConstants
 const omega_const_bf_ = Ref{BigFloat}()
 
 function __init__()
+    # allocate storage for this BigFloat constant each time this module is loaded
     omega_const_bf_[] =
         parse(BigFloat,"0.5671432904097838729999686622103555497538157871865125081351310792230457930866845666932194")
 end
